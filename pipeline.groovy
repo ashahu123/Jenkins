4f19586ef9e9dd56be494dce6ccce350b56f8efb
ashahu123 <aniketshahu004@gmail.com>
2024-12-24 18:42:51 +0530

    commit

--- pipeline.groovy
+++ pipeline.groovy
@@ -56,3 +56,22 @@ pipeline {
 }
 
 ---
+
+Scripted pipeline:
+
+node {
+    stage('PULL') {
+        git branch: 'main', url: 'https://github.com/ashahu123/HelloWorld1.git'
+    }
+    stage('Build') {
+        echo 'Congrats...Build Success...!!!'
+    }
+    stage('Test') {
+        echo 'Congrats...Test Success...!!!'
+    }
+    stage('Deploy') {
+        echo 'Congrats...Deploy Success...!!!'
+    }
+}
+
+---
