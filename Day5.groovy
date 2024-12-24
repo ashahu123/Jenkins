bc8b1c6c547c78c35a3927e4c6d72c834729c923
ashahu123 <aniketshahu004@gmail.com>
2024-12-24 18:47:29 +0530

    new commit

---
+++ Day5.groovy
@@ -0,0 +1,23 @@
+Day5.txt
+ *Scripted Pipeline:
+ - How to write
+ - Pull stage using scripted pipeline
+
+
+ Scripted pipeline:
+
+node {
+    stage('PULL') {
+        git branch: 'main', url: 'https://github.com/ashahu123/HelloWorld1.git'
+    }
+    stage('Build') {
+        echo 'Congrats...Build Success...!!!'
+    }
+    stage('Test') {
+        echo 'Congrats...Test Success...!!!'
+    }
+    stage('Deploy') {
+        echo 'Congrats...Deploy Success...!!!'
+    }
+}
+
